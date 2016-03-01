14a35fb620437012b980ba4910420b77245953b0
Thameem Syed <sdthameem@gmail.com>
2016-02-29 21:54:51 -0500

    adding new brand report logic

--- app.rb
+++ app.rb
@@ -1,53 +1,94 @@
 
+##**************Printing generic data section********************************************
 def print_sales_report_art
-     puts"\n********************************************************************************"            
-    puts"  #####                                 ######                                     " 
-    puts" #     #   ##   #      ######  ####     #     # ###### #####   ####  #####  #####  "
-    puts" #        #  #  #      #      #         #     # #      #    # #    # #    #   #    "
-    puts"  #####  #    # #      #####   ####     ######  #####  #    # #    # #    #   #    "
-    puts"       # ###### #      #           #    #   #   #      #####  #    # #####    #    "
-    puts" #     # #    # #      #      #    #    #    #  #      #      #    # #   #    #    "
-    puts"  #####  #    # ###### ######  ####     #     # ###### #       ####  #    #   #    "
-    puts"********************************************************************************\n "            
+    $report_file.puts"\n********************************************************************************"            
+    $report_file.puts"  #####                                 ######                                     " 
+    $report_file.puts" #     #   ##   #      ######  ####     #     # ###### #####   ####  #####  #####  "
+    $report_file.puts" #        #  #  #      #      #         #     # #      #    # #    # #    #   #    "
+    $report_file.puts"  #####  #    # #      #####   ####     ######  #####  #    # #    # #    #   #    "
+    $report_file.puts"       # ###### #      #           #    #   #   #      #####  #    # #####    #    "
+    $report_file.puts" #     # #    # #      #      #    #    #    #  #      #      #    # #   #    #    "
+    $report_file.puts"  #####  #    # ###### ######  ####     #     # ###### #       ####  #    #   #    "
+    $report_file.puts"********************************************************************************\n "            
 end
 
 def print_todays_date 
 	# Print today's date
-	puts "Date : #{Time.now.strftime("%m/%d/%Y")}"
-end
-
-
-def increment_counter(counter)
-	counter += 1
-	return counter
-end
-
-def sum(options = {})
-	if options[:sum] and options[:sum1] and options[:sum2]
-		options[:sum] +=  options[:sum1] + options[:sum2]
-		return options[:sum]
-	end
-	if options[:sum] and options[:sum1]
-		options[:sum] +=  options[:sum1]
-		return options[:sum]
-	end
-end
-
-def calculate_avg_price(revenue,count)
-	avg_price = revenue / count
-	return avg_price
+	$report_file.puts "Date : #{Time.now.strftime("%m/%d/%Y")}"
 end
 
 def print_products_in_art
-	puts "                     _            _       "
-    puts "                    | |          | |      "
-    puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
-    puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-    puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-    puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-    puts "| |                                       "
-    puts "|_|                                       \n\n"
+	$report_file.puts "                     _            _       "
+    $report_file.puts "                    | |          | |      "
+    $report_file.puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+    $report_file.puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+    $report_file.puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+    $report_file.puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+    $report_file.puts "| |                                       "
+    $report_file.puts "|_|                                       \n\n"
 end
+
+def print_brands_in_art
+    $report_file.puts " _                         _     "
+    $report_file.puts "| |                       | |    "
+    $report_file.puts "| |__  _ __ __ _ _ __   __| |___ "
+    $report_file.puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+    $report_file.puts "| |_) | | | (_| | | | | (_| \\__ \\"
+    $report_file.puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+    $report_file.puts
+end
+
+def print_products_report(toy_name,full_price,product_purchase_cnt,sales_sum,avg_price,avg_discount)
+    $report_file.puts toy_name
+    $report_file.puts "*********************************"
+    $report_file.puts "Retail price     : #{full_price}"
+    $report_file.puts "Product purchases: #{product_purchase_cnt}"
+    $report_file.puts "Product Sales    : #{sales_sum}"
+    $report_file.puts "Average price    : #{avg_price.round(2)}"
+    $report_file.puts "Average Discount : $#{avg_discount.round(2)} \n\n"  
+end
+
+
+def temp_array_brand_report
+    temp_array = [brand_name,toys_in_stock,brand_avg_price,brand_revenue]
+    return temp_array
+end
+
+#def print_brands_report(brand_name,toys_in_stock, brand_avg_price,brand_revenue)
+   
+#   $report_file.puts brand_name
+#   $report_file.puts "*********************************"
+#   $report_file.puts "Toys in Stock: #{toys_in_stock}"
+#   $report_file.puts "Average price: #{brand_avg_price.round(2)}"
+#   $report_file.puts "Total Revenue: #{brand_revenue.round(2)} \n\n"
+#end
+
+##**************Reusable methods section-start********************************************
+
+def increment_counter(counter)
+    counter += 1
+    return counter
+end
+
+def sum(options = {})
+    if options[:sum] and options[:sum1] and options[:sum2]
+        options[:sum] +=  options[:sum1] + options[:sum2]
+        return options[:sum]
+    end
+    if options[:sum] and options[:sum1]
+        options[:sum] +=  options[:sum1]
+        return options[:sum]
+    end
+end
+
+def calculate_avg_price(revenue,count)
+    avg_price = revenue / count
+    return avg_price
+end
+
+##**************Reusable methods section-end********************************************
+
+##**************Calculate reveue and count for products and brand-start********************************************
 
 def get_product_sales_count(toy_name,sales_sum,product_purchase_cnt)
 	toy_name["purchases"].each do |purchase_num|
@@ -57,14 +98,31 @@ def get_product_sales_count(toy_name,sales_sum,product_purchase_cnt)
     return sales_sum,product_purchase_cnt
 end
 
-def print_products_report(toy_name,full_price,product_purchase_cnt,sales_sum,avg_price,avg_discount)
-	puts toy_name
-    puts "*********************************"
-    puts "Retail price     : #{full_price}"
-    puts "Product purchases: #{product_purchase_cnt}"
-    puts "Product Sales    : #{sales_sum}"
-    puts "Average price    : #{avg_price.round(2)}"
-    puts "Average Discount : $#{avg_discount.round(2)} \n\n"  
+def get_brand_revenue_count(brand_key_details,brand_revenue,brand_purchase_cnt)
+	brand_key_details["purchases"].each do |purchase_brand|
+       brand_purchase_cnt = increment_counter(brand_purchase_cnt)
+       brand_revenue = sum(sum: brand_revenue , sum1: purchase_brand["price"] , sum2: purchase_brand["shipping"] )
+    end
+    return brand_revenue , brand_purchase_cnt
+end
+
+##**************Calculate reveue and count for products and brand-end********************************************
+
+##**************Main method for Products and Brands-start********************************************
+
+def brands_report(brand_name,brand_details)
+# Initializing variables
+    toys_in_stock = 0
+    brand_revenue,brand_avg_price, brand_purchase_cnt = 0.0, 0.0, 0
+# Fetching each value details in a brand and doing calculation
+  brand_details.each do |brand_key_details|     
+    toys_in_stock = sum(sum: toys_in_stock, sum1: brand_key_details["stock"])
+    brand_revenue , brand_purchase_cnt = get_brand_revenue_count(brand_key_details,brand_revenue,brand_purchase_cnt)
+#Calculating average price 
+    brand_avg_price = calculate_avg_price(brand_revenue,brand_purchase_cnt)
+  end
+# Printing the required data for each brand
+#  print_brands_report(brand_name,toys_in_stock, brand_avg_price,brand_revenue)
 end
 
 def products_report(product_details)
@@ -86,49 +144,13 @@ def products_report(product_details)
         print_products_report(toy_name["title"],toy_name["full-price"],product_purchase_cnt,sales_sum,avg_price,avg_discount)
     end
 end
+##**************Main method for Products and Brands-end********************************************
 
-def print_brands_report(brand_name,toys_in_stock, brand_avg_price,brand_revenue)
-	puts brand_name
-    puts "*********************************"
-    puts "Toys in Stock: #{toys_in_stock}"
-    puts "Average price: #{brand_avg_price.round(2)}"
-    puts "Total Revenue: #{brand_revenue.round(2)} \n\n"
-end
-
-def print_brands_in_art
-    puts " _                         _     "
-	puts "| |                       | |    "
-	puts "| |__  _ __ __ _ _ __   __| |___ "
-	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-	puts "| |_) | | | (_| | | | | (_| \\__ \\"
-	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
-	puts
-end
-
-def get_brand_revenue_count(brand_key_details,brand_revenue,brand_purchase_cnt)
-	brand_key_details["purchases"].each do |purchase_brand|
-       brand_purchase_cnt = increment_counter(brand_purchase_cnt)
-       brand_revenue = sum(sum: brand_revenue , sum1: purchase_brand["price"] , sum2: purchase_brand["shipping"] )
-    end
-    return brand_revenue , brand_purchase_cnt
-end
-
-
-def brands_report(brand_name,brand_details)
-# Initializing variables
-    toys_in_stock = 0
-    brand_revenue,brand_avg_price, brand_purchase_cnt = 0.0, 0.0, 0
-# Fetching each value details in a brand and doing calculation
-  brand_details.each do |brand_key_details|  	
-    toys_in_stock = sum(sum: toys_in_stock, sum1: brand_key_details["stock"])
-    brand_revenue , brand_purchase_cnt = get_brand_revenue_count(brand_key_details,brand_revenue,brand_purchase_cnt)
-#Calculating average price 
-    brand_avg_price = calculate_avg_price(brand_revenue,brand_purchase_cnt)
-  end
-# Printing the required data for each brand
-  print_brands_report(brand_name,toys_in_stock, brand_avg_price,brand_revenue)
-end
-
+###Creating a new array with group by brands and then fetching each hash value pair-start
+def create_group_array
+    group_array= $products_hash["items"].group_by {|name| name["brand"]}
+    return group_array
+end 
 
 def use_group_array(group_array)
 	group_array.each_with_index do |(name,details),index|
@@ -142,14 +164,9 @@ def use_group_array(group_array)
         brands_report(name,details)
     end
 end
+###Creating a new array with group by brands and then fetching each hash value pair-end
 
-
-def create_group_array
-	group_array= $products_hash["items"].group_by {|name| name["brand"]}
-	return group_array
-end    
-
-
+###Print main heading and call methods to get Products and Brand details
 def create_report
 	print_sales_report_art
 	print_todays_date
@@ -157,7 +174,7 @@ def create_report
 	use_group_array(group_array)
 end
 
-
+###Parsing file method
 def setup_files
     require 'json'
 	path = File.join(File.dirname(__FILE__), '../data/products.json')
@@ -166,7 +183,7 @@ def setup_files
     $report_file = File.new("report.txt", "w+")
 end
 
-
+##Start method
 def start
 	setup_files
 	create_report
